f695ed4a1e0f378edc29b281100315670f9bc010
21LUCI <91661502+21LUCI@users.noreply.github.com>
2021-12-10 23:24:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,17 @@
+//
+// Created by renyi on 10/12/2021
+//
+
 #include "lib.h"
 float numeroMAssimo( const float numeri[], int dim){
     numeri[dim] ;
    float massimo=numeri[0];
   for(int i=0; i<dim; i++){
-    if(massimo<numeri[i]{
+    if(massimo<numeri[i]){
       massimo=numeri[i];
        }
 }
 return massimo;
+    
 }
    
