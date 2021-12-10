985ba476a6d492a39fa10a8b31c0369934e52216
21LUCI <91661502+21LUCI@users.noreply.github.com>
2021-12-10 23:25:50 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,8 @@
-#ifndf UNTITLED15_LIB_H
+//
+// Created by renyi on 10/12/2021
+//
+
+#ifndef UNTITLED15_LIB_H
 #define UNTITLED15_LIB_H
 float numeroMassimo(const float numeri[], int dimensione);
 
